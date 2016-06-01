4a0e2eeabed54d33d22ccbd81654b1bab4c378fd
Luca Tumedei <luca@theaveragedev.com>
2016-06-01 16:06:08 +0200

    avoid registering meta boxes twice, check for handler

--- src/Post_Type/Post_Type_Registration.php
+++ src/Post_Type/Post_Type_Registration.php
@@ -2,7 +2,6 @@
 
 namespace Tribe\Libs\Post_Type;
 
-use Tribe\Libs\Post_Type\Meta_Box_Handlers\CMB2 as CMB2_Meta_Box_Handler;
 use Tribe\Libs\Post_Type\Meta_Box_Handlers\Meta_Box_Handler_Interface;
 
 class Post_Type_Registration {
@@ -14,7 +13,6 @@ class Post_Type_Registration {
 	 * @param Meta_Box_Handler_Interface $meta_box_handler
 	 */
 	public static function register( Post_Type_Config $config, Meta_Box_Handler_Interface $meta_box_handler = null ) {
-		$meta_box_handler = null !== $meta_box_handler ? $meta_box_handler : new CMB2_Meta_Box_Handler();
 		$callback         = self::build_registration_callback( $config, $meta_box_handler );
 
 		// do not register until init
@@ -33,30 +31,33 @@ class Post_Type_Registration {
 	 *
 	 * @return \Closure
 	 */
-	private static function build_registration_callback( Post_Type_Config $config, Meta_Box_Handler_Interface $meta_box_handler ) {
+	private static function build_registration_callback( Post_Type_Config $config, Meta_Box_Handler_Interface $meta_box_handler = null ) {
 		return function () use ( $config, $meta_box_handler ) {
 			if ( empty( $config->post_type() ) ) {
 				throw new \RuntimeException( 'Invalid configuration. Specify a post type.' );
 			}
 			
 			register_extended_post_type( $config->post_type(), $config->get_args(), $config->get_labels() );
-			
-			add_filter( 'cmb2_meta_boxes', function( $meta_boxes ) use ( $config ) {
-				$post_type_meta_boxes = $config->get_meta_boxes();
-				$post_type_meta_boxes = apply_filters( "tribe_{$config->post_type()}_meta_boxes", $post_type_meta_boxes );
-				$meta_boxes = array_merge( $meta_boxes, $post_type_meta_boxes );
-				return $meta_boxes;
-			});
-			
-			// acf fires `acf/include_fields` too early to use a callback here, at init:5
-			if ( function_exists( 'acf_add_local_field_group' ) ) {
-				$meta_boxes = $config->get_acf_fields();
-				foreach ( $meta_boxes as $box ) {
-					acf_add_local_field_group( $box );
+
+			if ( $meta_box_handler !== null ) {
+				$meta_box_handler->register_meta_boxes( $config );
+			} else {
+				add_filter( 'cmb2_meta_boxes', function ( $meta_boxes ) use ( $config ) {
+					$post_type_meta_boxes = $config->get_meta_boxes();
+					$post_type_meta_boxes = apply_filters( "tribe_{$config->post_type()}_meta_boxes", $post_type_meta_boxes );
+					$meta_boxes           = array_merge( $meta_boxes, $post_type_meta_boxes );
+
+					return $meta_boxes;
+				} );
+
+				// acf fires `acf/include_fields` too early to use a callback here, at init:5
+				if ( function_exists( 'acf_add_local_field_group' ) ) {
+					$meta_boxes = $config->get_acf_fields();
+					foreach ( $meta_boxes as $box ) {
+						acf_add_local_field_group( $box );
+					}
 				}
 			}
-			
-			$meta_box_handler->register_meta_boxes( $config );
 		};
 	}
 }
