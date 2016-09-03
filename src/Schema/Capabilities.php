e8cb42031a542846f89246905a8a7a83ea71f687
Jonathan Brinley <jbrinley@flightless.us>
2016-09-02 22:37:42 -0400

    more robust registration of post type capabilities
    The prior version did not handle post types with custom values
    for the 'capabilities' property, and required pluralization
    by string concatenation in the caller.
    
    The new version uses the actually capabilities assigned
    to the post type, and can use the real post ID as an argument.
    
    I've maintained the prior version for backwards compatibility,
    in the event that the method is called with the capability
    suffix rather than the post type name.

--- src/Schema/Capabilities.php
+++ src/Schema/Capabilities.php
@@ -11,52 +11,93 @@ namespace Tribe\Libs\Schema;
  * A utililty class for registering capabilities for a post type
  */
 class Capabilities {
-	private $cap_prefixes = array(
-		'editor' => array(
-			'create',
+	private $caps = [
+		'editor'      => [
 			'read',
-			'read_private',
-			'edit',
-			'edit_others',
-			'edit_private',
-			'edit_published',
-			'delete',
-			'delete_others',
-			'delete_private',
-			'delete_published',
-			'publish',
-		),
-		'author' => array(
-			'create',
+			'read_private_posts',
+			'create_posts',
+			'edit_posts',
+			'edit_others_posts',
+			'edit_private_posts',
+			'edit_published_posts',
+			'delete_posts',
+			'delete_others_posts',
+			'delete_private_posts',
+			'delete_published_posts',
+			'publish_posts',
+		],
+		'author'      => [
 			'read',
-			'edit',
-			'edit_published',
-			'delete',
-			'delete_published',
-			'publish',
-		),
-		'contributor' => array(
-			'create',
+			'create_posts',
+			'edit_posts',
+			'edit_published_posts',
+			'delete_posts',
+			'delete_published_posts',
+			'publish_posts',
+		],
+		'contributor' => [
 			'read',
-			'edit',
-			'delete',
-		),
-		'subscriber' => array(
+			'create_posts',
+			'edit_posts',
+			'delete_posts',
+		],
+		'subscriber'  => [
 			'read',
-		),
-	);
+		],
+	];
 
+	/**
+	 * Register capabilities to a role for a post type
+	 *
+	 * @param string $post_type The name of a registered post type
+	 * @param string $role_id   The name of a registered role
+	 * @param string $level     The access level the user should have. One of 'subscriber', 'contributor', 'author', or
+	 *                          'editor'
+	 * @return bool
+	 */
 	public function register_post_type_caps( $post_type, $role_id, $level = 'editor' ) {
-		if ( !isset( $this->cap_prefixes[$level] ) ) {
-			return FALSE;
+		if ( ! isset( $this->caps[ $level ] ) ) {
+			return false;
 		}
+
 		$role = get_role( $role_id );
-		if ( !$role ) {
-			return FALSE;
+		if ( ! $role ) {
+			return false;
 		}
-		foreach ( $this->cap_prefixes[$level] as $prefix ) {
-			$role->add_cap( $prefix . '_' . $post_type );
+
+		$pto = get_post_type_object( $post_type );
+		if ( ! $pto ) {
+			_deprecated_argument( __FUNCTION__, '2016-09-02', 'First argument should be a registered post type, rather than the plural capability prefix.' );
+			return $this->register_post_type_caps_with_prefix( $post_type, $role_id, $level );
 		}
-		return TRUE;
+
+		foreach ( $this->caps[ $level ] as $cap ) {
+			$role->add_cap( $pto->cap->{$cap} );
+		}
+
+		return true;
+	}
+
+	/**
+	 * Provided for backwards compatibility. Registers post type capabilities
+	 * using the plural post type slug as a suffix.
+	 *
+	 * Not recommended, because it does not take into account custom capabilities
+	 * set for the post type when it is registered.
+	 *
+	 * @param string $post_type_suffix The plural form of the post type's capability_type
+	 * @param string $role_id          The name of a registered role
+	 * @param string $level            The access level the user should have. One of 'subscriber', 'contributor',
+	 *                                 'author', or 'editor'
+	 * @return bool
+	 */
+	private function register_post_type_caps_with_prefix( $post_type_suffix, $role_id, $level = 'editor' ) {
+		// argument validation handled above in self::register_post_type_caps()
+		$role = get_role( $role_id );
+		foreach ( $this->caps[ $level ] as $cap ) {
+			$prefix = str_replace( '_posts', '', $cap );
+			$role->add_cap( $prefix . '_' . $post_type_suffix );
+		}
+		return true;
 	}
 } 
