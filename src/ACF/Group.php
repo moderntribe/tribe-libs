9461ab45504cd407a20f922ffd2af107e780d301
Jonathan Brinley <jbrinley@flightless.us>
2017-01-02 10:41:18 -0500

    handle attachment post type in acf registration
    ACF uses a different formula for adding fields to attachment post types vs
    all other post types. The ACF\Group can mask that distinction.

--- src/ACF/Group.php
+++ src/ACF/Group.php
@@ -22,13 +22,23 @@ class Group extends ACF_Configuration implements ACF_Aggregate {
 		}
 
 		foreach ( $this->post_types as $post_type ) {
-			$attributes[ 'location' ][] = [
-				[
-					'param'    => 'post_type',
-					'operator' => '==',
-					'value'    => $post_type,
-				],
-			];
+			if ( $post_type === 'attachment' ) {
+				$attributes[ 'location' ][] = [
+					[
+						'param'    => 'attachment',
+						'operator' => '==',
+						'value'    => 'all',
+					],
+				];
+			} else {
+				$attributes[ 'location' ][] = [
+					[
+						'param'    => 'post_type',
+						'operator' => '==',
+						'value'    => $post_type,
+					],
+				];
+			}
 		}
 		return $attributes;
 	}
