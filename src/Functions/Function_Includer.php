096387f4b818ac7083d3e211b40294cabc866d16
Jonathan Brinley <jbrinley@flightless.us>
2019-04-18 12:23:07 -0400

    move cache functions to autoloader

--- src/Functions/Function_Includer.php
+++ src/Functions/Function_Includer.php
@@ -5,9 +5,6 @@ namespace Tribe\Libs\Functions;
 
 
 abstract class Function_Includer {
-	public static function cache() {
-		require_once( __DIR__ . '/cache.php' );
-	}
 
 	public static function version() {
 		require_once( __DIR__ . '/version.php' );
