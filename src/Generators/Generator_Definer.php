02c6a755c21a992bce0c1824ac7c7166fea32787
Jonathan Brinley <jbrinley@flightless.us>
2020-03-25 20:45:50 -0400

    adapt generator provider for php-di

---
+++ src/Generators/Generator_Definer.php
@@ -0,0 +1,37 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Libs\Generators;
+
+use DI;
+use Psr\Container\ContainerInterface;
+use Tribe\Libs\Container\Definer_Interface;
+
+class Generator_Definer implements Definer_Interface {
+	public const PATH     = 'generator.path';
+	public const COMMANDS = 'generator.commands';
+
+	public function define(): array {
+		return [
+			self::PATH => function ( ContainerInterface $container ) {
+				return plugin_dir_path( $container->get( 'plugin.file' ) ) . 'src';
+			},
+
+			File_System::class => DI\create(),
+
+			CLI_Generator::class      => DI\create()->constructor( DI\get( File_System::class ), DI\get( self::PATH ) ),
+			CPT_Generator::class      => DI\create()->constructor( DI\get( File_System::class ), DI\get( self::PATH ) ),
+			Settings_Generator::class => DI\create()->constructor( DI\get( File_System::class ), DI\get( self::PATH ) ),
+			Taxonomy_Generator::class => DI\create()->constructor( DI\get( File_System::class ), DI\get( self::PATH ) ),
+			Meta_Importer::class      => DI\create()->constructor( DI\get( File_System::class ), DI\get( self::PATH ) ),
+
+			self::COMMANDS => [
+				DI\get( CLI_Generator::class ),
+				DI\get( CPT_Generator::class ),
+				DI\get( Settings_Generator::class ),
+				DI\get( Taxonomy_Generator::class ),
+				DI\get( Meta_Importer::class ),
+			],
+		];
+	}
+}
