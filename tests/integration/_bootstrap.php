e8a41bbacedfab6ddc96470087369b832ebd2ca9
Jonathan Brinley <jbrinley@flightless.us>
2020-04-21 09:26:16 -0400

    set REQUEST_TIME_FLOAT to a float before running tests

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,2 +1,9 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+
+// workaround for an oddity when running tests on GH actions with php-timer 3.1.4+
+if ( ! isset( $_SERVER['REQUEST_TIME_FLOAT'] ) ) {
+	$_SERVER['REQUEST_TIME_FLOAT'] = microtime( true );
+} elseif ( ! \is_float( $_SERVER['REQUEST_TIME_FLOAT'] ) ) {
+	$_SERVER['REQUEST_TIME_FLOAT'] = (float) $_SERVER['REQUEST_TIME_FLOAT'];
+}
