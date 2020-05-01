59437cbef5d0547ad33afb2777485913da6fe937
Jonathan Brinley <jbrinley@flightless.us>
2020-04-30 22:23:33 -0400

    migrate media utilities from core plugin
    - WP responsive image disabler
    - Full size GIFs

---
+++ src/Media/Full_Size_Gif.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tribe\Libs\Media;
+
+class Full_Size_Gif {
+
+	/**
+	 * @param array|false $data
+	 * @param int         $attachment_id
+	 * @param string      $size
+	 *
+	 * @return array|false
+	 * @filter image_downsize
+	 */
+	public function full_size_only_gif( $data, $attachment_id, $size ) {
+		if ( $size === 'full' ) {
+			return $data;
+		}
+		$meta = wp_get_attachment_metadata( $attachment_id );
+		if ( isset( $meta['file'] ) && $this->is_gif( $meta['file'] ) ) {
+			return image_downsize( $attachment_id, 'full' );
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param $src
+	 *
+	 * @return bool
+	 */
+	public function is_gif( $src ): bool {
+		return ( pathinfo( $src, PATHINFO_EXTENSION ) === 'gif' );
+	}
+
+}
