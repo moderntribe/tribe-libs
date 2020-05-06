7c8df7b781c268f8866d7e7450bd42a0bfbfe288
jfrydman <p69alk881muppets>
2020-05-05 21:56:13 -0600

    Add hook based logging system

---
+++ src/Log/Log_Actions.php
@@ -0,0 +1,57 @@
+<?php declare( strict_types=1 );
+
+namespace Tribe\Libs\Log;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Log_Actions
+ *
+ * Log something with WordPress actions
+ *
+ * do_action( Logger_Actions::WARNING, 'Something is wrong!', [ 'data' = $data ] );
+ *
+ * @package Tribe\Libs\Log
+ */
+class Log_Actions {
+
+	const EMERGENCY = 'tribe/log/emergency';
+	const ALERT     = 'tribe/log/alert';
+	const CRITICAL  = 'tribe/log/critical';
+	const ERROR     = 'tribe/log/error';
+	const WARNING   = 'tribe/log/warning';
+	const NOTICE    = 'tribe/log/notice';
+	const INFO      = 'tribe/log/info';
+	const DEBUG     = 'tribe/log/debug';
+
+	/**
+	 * The logger instance
+	 *
+	 * @var LoggerInterface
+	 */
+	protected $logger;
+
+	/**
+	 * Log_Actions constructor.
+	 *
+	 * @param   LoggerInterface  $logger
+	 */
+	public function __construct( LoggerInterface $logger ) {
+		$this->logger = $logger;
+	}
+
+	/**
+	 * Initialize the actions
+	 */
+	public function init() {
+		add_action( self::EMERGENCY, [ $this->logger, 'emergency' ], 10, 2 );
+		add_action( self::ALERT, [ $this->logger, 'alert' ], 10, 2 );
+		add_action( self::CRITICAL, [ $this->logger, 'critical' ], 10, 2 );
+		add_action( self::ERROR, [ $this->logger, 'error' ], 10, 2 );
+		add_action( self::WARNING, [ $this->logger, 'warning' ], 10, 2 );
+		add_action( self::NOTICE, [ $this->logger, 'notice' ], 10, 2 );
+		add_action( self::INFO, [ $this->logger, 'info' ], 10, 2 );
+		add_action( self::DEBUG, [ $this->logger, 'debug' ], 10, 2 );
+	}
+
+}
---
+++ src/Log/Logger.php
@@ -0,0 +1,169 @@
+<?php declare( strict_types=1 );
+
+namespace Tribe\Libs\Log;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger as MonoLogger;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Logger
+ *
+ * Log to files via Monolog
+ *
+ * @package Tribe\Libs\Log
+ */
+class Logger implements LoggerInterface {
+
+	/**
+	 * The MonoLogger Instance
+	 *
+	 * @var MonoLogger
+	 */
+	protected $log;
+
+	/**
+	 * Logger constructor.
+	 *
+	 * @param   AbstractProcessingHandler  $handler  The Monolog handler
+	 * @param   string                     $name     The name of your logger
+	 */
+	public function __construct( AbstractProcessingHandler $handler, string $name = 'square-one' ) {
+		$this->log = new MonoLogger( $name );
+		$this->log->pushHandler( $handler );
+	}
+
+	/**
+	 * Adds additional push handlers
+	 *
+	 * @param   AbstractProcessingHandler  $handler
+	 *
+	 * @return LoggerInterface
+	 */
+	public function add_push_handler( AbstractProcessingHandler $handler ): LoggerInterface {
+		return $this->log->pushHandler( $handler );
+	}
+
+	/**
+	 * System is unusable.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function emergency( $message, array $context = [] ) {
+		$this->log->emergency( $message, $context );
+	}
+
+	/**
+	 * Action must be taken immediately.
+	 *
+	 * Example: Entire website down, database unavailable, etc. This should
+	 * trigger the SMS alerts and wake you up.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function alert( $message, array $context = [] ) {
+		$this->log->alert( $message, $context );
+	}
+
+	/**
+	 * Critical conditions.
+	 *
+	 * Example: Application component unavailable, unexpected exception.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function critical( $message, array $context = [] ) {
+		$this->log->critical( $message, $context );
+	}
+
+	/**
+	 * Runtime errors that do not require immediate action but should typically
+	 * be logged and monitored.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function error( $message, array $context = [] ) {
+		$this->log->error( $message, $context );
+	}
+
+	/**
+	 * Exceptional occurrences that are not errors.
+	 *
+	 * Example: Use of deprecated APIs, poor use of an API, undesirable things
+	 * that are not necessarily wrong.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function warning( $message, array $context = [] ) {
+		$this->log->warning( $message, $context );
+	}
+
+	/**
+	 * Normal but significant events.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function notice( $message, array $context = [] ) {
+		$this->log->notice( $message, $context );
+	}
+
+	/**
+	 * Interesting events.
+	 *
+	 * Example: User logs in, SQL logs.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function info( $message, array $context = [] ) {
+		$this->log->info( $message, $context );
+	}
+
+	/**
+	 * Detailed debug information.
+	 *
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 */
+	public function debug( $message, array $context = [] ) {
+		$this->log->debug( $message, $context );
+	}
+
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param   mixed   $level
+	 * @param   string  $message
+	 * @param   array   $context
+	 *
+	 * @return void
+	 *
+	 * @throws \Psr\Log\InvalidArgumentException
+	 */
+	public function log( $level, $message, array $context = [] ) {
+		$this->log->log( $level, $message, $context );
+	}
+
+}
