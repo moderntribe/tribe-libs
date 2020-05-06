2321574cc7670d14204b63d448f108eb3658e0dd
defunctl <justin.frydman@gmail.com>
2020-05-06 16:26:35 -0600

    Remove pimple provider, add log definer + subscriber

---
+++ src/Log/Log_Definer.php
@@ -0,0 +1,30 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Libs\Log;
+
+use DI;
+use Monolog\Logger as MonoLogger;
+use Monolog\Handler\StreamHandler;
+use MHCG\Monolog\Handler\WPCLIHandler;
+use Tribe\Libs\Container\Definer_Interface;
+
+class Log_Definer implements Definer_Interface {
+
+	public function define(): array {
+		return [
+			Psr\Log\LoggerInterface::class => DI\factory( function () {
+				$path        = WP_CONTENT_DIR . '/square-one-' . date( 'Y-m-d' ) . '.log';
+				$path        = apply_filters( 'tribe/log/path', $path );
+				$handler     = new StreamHandler( $path, MonoLogger::DEBUG );
+				$cli_handler = new WPCLIHandler( MonoLogger::DEBUG );
+
+				$logger = new Logger( $handler );
+				$logger->add_push_handler( $cli_handler );
+
+				return $logger;
+			} ),
+		];
+	}
+
+}
---
+++ src/Log/Log_Subscriber.php
@@ -0,0 +1,22 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tribe\Libs\Cache;
+
+use Tribe\Libs\Container\Abstract_Subscriber;
+use Tribe\Libs\Log\Log_Actions;
+
+class Log_Subscriber extends Abstract_Subscriber {
+
+	public function register(): void {
+		$this->log();
+	}
+
+	protected function log(): void {
+		// Set logger action hooks
+		add_action( 'init', static function () {
+			$this->container->get( Log_Actions::class )->init();
+		}, 0, 0 );
+	}
+
+}
