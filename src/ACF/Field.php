4b12c05437c61b26f57748f76f09ac2c97203b65
Mike Sawicki <mikes@tri.be>
2020-07-31 14:46:22 -0400

    Moves all the blocks lib work over, adds collection to Utils

--- src/ACF/Field.php
+++ src/ACF/Field.php
@@ -5,4 +5,9 @@ namespace Tribe\Libs\ACF;
 
 class Field extends ACF_Configuration {
 	protected $key_prefix = 'field';
+
+	public function __construct( $key, $attributes = []) {
+		$this->attributes = $attributes;
+		parent::__construct( $key );
+	}
 }
