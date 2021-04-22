744a245b8cd10bab01a4e12ba74b5503694dc496
Matthew McAchran <mattmcachran@gmail.com>
2021-04-22 08:51:12 -0400

    Feature/routing (#70)
    * Adds base routing classes.
    
    * Cleanup.
    
    * Remove route repository class.
    
    * Cleanup.
    
    * Updates how routes and rest routes are registered.
    
    * Updates routing logic to pull from the DI container when registering routes.
    
    * Cleanup.
    
    * Cleanup.
    
    * Simplify load logic.
    
    * Comment cleanup.
    
    * Updates abstract class for REST routes.
    
    * Adds sample routes to tribe-libs.
    
    * Composer updates.`
    
    * Updates composer lock file.
    
    * Composer updates.
    
    * PHPCS cleanup.
    
    * PHPCS cleanup.
    
    * PHPCS cleanup.
    
    * PHPCS cleanup.
    
    * Cleanup.
    
    * PHPCS cleanup.
    
    * Spacing cleanup.
    
    * Cleanup.
    
    * Cleanup.
    
    * CLeanup.
    
    * Cleanup.
    
    * Cleanup.
    
    * Fix failing tests.
    
    * Copypasta fix for tests.
    
    * Updates class names.
    
    * Fix tests.
    
    * Force composer to use v1.
    
    * Force composer v1 in Analyze step.
    
    * Use composer v1 instead of v2.
    
    * Remove composer self-update since it's added to the list of tools for the CI steps.
    
    * Remove sample routes from tribe-libs in favor of adding to the Sample CPT in Square1.
    
    * PHPCS cleanup.
    
    * Update src/Routes/Abstract_Route.php
    
    Co-authored-by: Justin <defunctl@users.noreply.github.com>
    
    * PHPCS cleanup.
    
    * Router class doesn't need to extend the Abstract_Subscriber and PHPCS cleanup.
    
    * PHPCS cleanup.
    
    * PHPCS cleanup.
    
    * Remove header since wp_die will treat the second parameter as the response code since it's an integer.
    
    * PHPCS cleanup.
    
    * Remove unused method.
    
    * Allow priority to be set on routes.
    
    * Updates routing logic.
    
    * Adds query var logic into rewrite rule manager class.
    
    * Comment cleanup.
    
    * Updates changelog.
    
    * Adds readme.
    
    Co-authored-by: Justin <defunctl@users.noreply.github.com>

---
+++ src/Routes/Router_Factory.php
@@ -0,0 +1,95 @@
+<?php declare( strict_types=1 );
+/**
+ * Class that is responsible for registering routes.
+ *
+ * @package Tribe\Project\Routes
+ */
+
+namespace Tribe\Libs\Routes;
+
+use Tribe\Libs\Routes\Abstract_Route;
+
+/**
+ * Class to register routers for normal and REST API endpoints.
+ */
+class Router_Factory {
+	/**
+	 * Currently matched route.
+	 *
+	 * @var Abstract_Route|string
+	 */
+	public $matched_route;
+
+	/**
+	 * List of Route Instances.
+	 *
+	 * @var array
+	 */
+	public $routes;
+
+	/**
+	 * List of router vars - tied to query vars.
+	 *
+	 * @var array
+	 */
+	public $router_vars;
+
+	/**
+	 * Instance of the rewrite rule manager.
+	 *
+	 * @var Router_Rule_Manager
+	 */
+	public $manager = null;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @return void
+	 */
+	public function __construct( Router_Rule_Manager $manager ) {
+		$this->manager = $manager;
+	}
+
+	/**
+	 * Looks up the custom route for the pattern matched. Returns false
+	 * if not found.
+	 *
+	 * @param string $pattern      The regex pattern to lookup.
+     * @param array $registered_routes Routes registered.
+	 * @return Abstract_Route|null The route or null on failure.
+	 */
+	public function find_route( $pattern, $registered_routes ): ?Abstract_Route {
+		// Load routes if not already loaded.
+		if ( empty( $this->routes ) ) {
+			$this->routes = $this->manager->get_route_objects( $registered_routes );
+		}
+
+		// Bail early if the route pattern doesn't exist.
+		if ( empty( $this->routes[ $pattern ] ) ) {
+			return null;
+		}
+
+		return $this->routes[ $pattern ];
+	}
+
+	/**
+	 * Checks if the current route is a custom route and activates it if matched.
+	 *
+	 * @hook parse_request
+	 *
+	 * @param \WP $wp The global wp object.
+     * @param array $registered_routes Routes registered.
+	 * @return Abstract_Route|null The matched route on success, null on failure.
+	 */
+	public function did_parse_request( \WP $wp, $registered_routes ): ?Abstract_Route {
+		$pattern       = $wp->matched_rule;
+		$matched_route = $this->find_route( $pattern, $registered_routes );
+
+		// Bail early if no matched route.
+		if ( empty( $matched_route ) ) {
+			return null;
+		}
+
+		return $matched_route;
+	}
+}
---
+++ src/Routes/Router_Rule_Manager.php
@@ -0,0 +1,178 @@
+<?php declare( strict_types=1 );
+/**
+ * Manages rewrite rules.
+ *
+ * @package Tribe\Project\Routes
+ */
+
+namespace Tribe\Libs\Routes;
+
+use Tribe\Libs\Routes\Abstract_Route;
+
+/**
+ * Class to manage rewrite rules.
+ */
+class Router_Rule_Manager {
+	/**
+	 * Currently matched route.
+	 *
+	 * @var Abstract_Route|string
+	 */
+	public $matched_route;
+
+	/**
+	 * List of Route Instances.
+	 *
+	 * @var array
+	 */
+	public $routes;
+
+	/**
+	 * Register REST API routes.
+	 *
+	 * @return void
+	 */
+	public function init_rest_routes( array $rest_routes = [] ): void {
+		// Register all REST routes defined.
+		foreach ( $rest_routes as $route ) {
+			$route->register();
+		}
+	}
+
+	/**
+	 * Merges the WordPress Rewrite Rules with the CS Rules.
+	 *
+	 * @hook rewrite_rules_array hook
+	 *
+	 * @param array $wp_rules          The WP rules array.
+     * @param array $registered_routes Routes registered.
+	 * @return array                   The modified rewrite rules array.
+	 */
+	public function load( $wp_rules = [], array $registered_routes = [] ): array {
+		$rules = $this->get_rules( $registered_routes );
+
+		// Loop through rules to determine where to add the rule.
+		foreach ( $rules as $pattern => $rule ) {
+			$wp_rules = ( 'top' === $rule['priority'] )
+				? [ $pattern => $rule['redirect'] ] + $wp_rules
+				: $wp_rules + [ $pattern => $rule['redirect'] ];
+		}
+
+		return $wp_rules;
+	}
+
+	/**
+	 * Converts Route instances into Rewrite rules for adding to
+	 * the WordPress rewrites
+	 *
+     * @param array $registered_routes Routes registered.
+	 * @return array Rules for the route instances.
+	 */
+	public function get_rules( array $registered_routes ): array {
+		$rules = [];
+
+		foreach ( $this->get_route_objects( $registered_routes ) as $pattern => $route ) {
+			// Skip routes that are defined in Core.
+			if ( $route->is_core() ) {
+				continue;
+			}
+
+			$matches  = $route->get_matches();
+			$redirect = 'index.php';
+
+			if ( ! empty( $matches ) ) {
+				$redirect .= '?' . $this->get_redirect_params( $matches );
+			}
+
+			$rules[ $pattern ] = [
+				'redirect' => $redirect,
+				'priority' => $route->get_priority(),
+			];
+		}
+
+		return $rules;
+	}
+
+	/**
+	 * Converts query params array into a query string. Special $matches
+	 * values are not URL encoded.
+	 *
+	 * @param array $params The query params.
+	 * @return string       Query string.
+	 */
+	public function get_redirect_params( array $params = [] ): string {
+		$query_params = [];
+
+		foreach ( $params as $key => $value ) {
+			if ( false === strpos( $value, '$matches' ) ) {
+				$query_params[] = urlencode( $key ) . '=' . urlencode( $value );
+			} else {
+				$query_params[] = urlencode( $key ) . '=' . $value;
+			}
+		}
+
+		return implode( '&', $query_params );
+	}
+
+    /**
+	 * Stores the route instances locally in an associative array on the
+	 * regex pattern. Any custom query vars are also scanned and stored
+	 * here.
+	 *
+     * @param array $registered_routes Routes registered.
+	 * @return array Route instances.
+	 */
+	public function get_route_objects( array $registered_routes = [] ): array {
+		$this->init_routes( $registered_routes );
+
+		return $this->routes;
+	}
+
+	/**
+	 * Lazy init routes and route vars.
+	 *
+     * @param array $registered_routes Routes registered.
+	 * @return void
+	 */
+	public function init_routes( array $registered_routes = [] ): void {
+		// Bail early if routes are already defined.
+		if ( ! empty( $this->routes ) ) {
+			return;
+		}
+
+		$this->routes      = [];
+		$this->router_vars = [];
+
+		// Register any routes defined.
+		foreach ( $registered_routes as $route ) {
+			$patterns   = $route->get_patterns();
+			$route_vars = $route->get_query_var_names();
+
+			// Add patterns to routes array.
+			foreach ( $patterns as $pattern ) {
+				$this->routes[ $pattern ] = $route;
+			}
+
+			$this->router_vars = array_merge( $this->router_vars, $route_vars );
+		}
+
+		$this->router_vars = array_unique( $this->router_vars );
+	}
+
+	/**
+	 * Adds the custom query vars.
+	 *
+	 * @hook query_vars
+	 *
+	 * @param array $query_vars WordPress query vars.
+	 * @return array            Modified query vars.
+	 */
+	public function did_query_vars( $query_vars ): array {
+		// Bail early if no query vars are defined for the router.
+		if ( empty( $this->router_vars ) ) {
+			return $query_vars;
+		}
+
+		return array_merge( $query_vars, $this->router_vars );
+	}
+}
