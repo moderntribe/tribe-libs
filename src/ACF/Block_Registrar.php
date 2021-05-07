eddb04c7a72a9c3eaf592cb8dd9fbd926267f283
Justin Frydman <justin.frydman@gmail.com>
2021-05-07 17:17:07 -0600

    Add filter when registering fields for a block, move field adding here

--- src/ACF/Block_Registrar.php
+++ src/ACF/Block_Registrar.php
@@ -1,5 +1,4 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\ACF;
 
@@ -20,9 +19,23 @@ class Block_Registrar {
 	}
 
 	/**
-	 * @param Block_Config $block_config
+	 * @param  Block_Config  $block_config
 	 */
 	protected function register_fields( Block_Config $block_config ) {
-		acf_add_local_field_group( $block_config->get_field_group()->get_attributes() );
+		$group = $block_config->get_field_group();
+
+		/**
+		 * Allow developers to modify a block's fields before being registered.
+		 *
+		 * @param \Tribe\Libs\ACF\Field[] $fields An array of Field instances.
+		 * @param \Tribe\Libs\ACF\Block_Config $block The block's instance.
+		 */
+		$fields = (array) apply_filters( 'tribe/block/register/fields', $block_config->get_fields(), $block_config );
+
+		foreach ( $fields as $field ) {
+			$group->add_field( $field );
+		}
+
+		acf_add_local_field_group( $group->get_attributes() );
 	}
 }
