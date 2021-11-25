d64abb51da959a410f1c13c64234629caadf6b94
Justin Frydman <justin.frydman@gmail.com>
2021-11-25 16:17:03 -0700

    Add pipeline contract

---
+++ src/Pipeline/Contracts/Pipeline.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Libs\Pipeline\Contracts;
+
+use Closure;
+
+interface Pipeline {
+
+	/**
+	 * Set the traveler object being sent on the pipeline.
+	 *
+	 * @param  mixed  $traveler
+	 *
+	 * @return $this
+	 */
+	public function send( $traveler ): Pipeline;
+
+	/**
+	 * Set the stops of the pipeline.
+	 *
+	 * @param  array|mixed  $stops
+	 *
+	 * @return $this
+	 */
+	public function through( $stops ): Pipeline;
+
+	/**
+	 * Set the method to call on the stops.
+	 *
+	 * @param  string  $method
+	 *
+	 * @return $this
+	 */
+	public function via( string $method ): Pipeline;
+
+	/**
+	 * Run the pipeline with a final destination callback.
+	 *
+	 * @param  \Closure  $destination
+	 *
+	 * @return mixed
+	 */
+	public function then( Closure $destination );
+
+}
