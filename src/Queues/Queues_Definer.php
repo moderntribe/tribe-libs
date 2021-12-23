59906dd192c6324446f0db6eecd3c94d6f9d1d25
Justin Frydman <justin.frydman@gmail.com>
2021-12-22 22:33:58 -0700

    Wire up stateful container

--- src/Queues/Queues_Definer.php
+++ src/Queues/Queues_Definer.php
@@ -1,11 +1,12 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\Queues;
 
 use DI;
 use Tribe\Libs\CLI\CLI_Definer;
+use Tribe\Libs\Container\Container;
 use Tribe\Libs\Container\Definer_Interface;
+use Tribe\Libs\Container\StatefulContainer;
 use Tribe\Libs\Queues\Backends\WP_Cache;
 use Tribe\Libs\Queues\CLI\Add_Tasks;
 use Tribe\Libs\Queues\CLI\Cleanup;
@@ -15,17 +16,19 @@ use Tribe\Libs\Queues\Contracts\Backend;
 use Tribe\Libs\Queues\Contracts\Queue;
 
 class Queues_Definer implements Definer_Interface {
+
 	public function define(): array {
 		return [
-			Backend::class          => DI\create( WP_Cache::class ),
-			Queue::class            => DI\autowire( DefaultQueue::class ),
-			Queue_Collection::class => DI\create()
+			StatefulContainer::class => DI\autowire( Container::class ),
+			Backend::class           => DI\create( WP_Cache::class ),
+			Queue::class             => DI\autowire( DefaultQueue::class ),
+			Queue_Collection::class  => DI\create()
 				->method( 'add', DI\get( Queue::class ) ),
 
 			/**
 			 * Add commands for the CLI subscriber to register
 			 */
-			CLI_Definer::COMMANDS => DI\add( [
+			CLI_Definer::COMMANDS    => DI\add( [
 				DI\get( List_Queues::class ),
 				DI\get( Add_Tasks::class ),
 				DI\get( Cleanup::class ),
@@ -33,4 +36,5 @@ class Queues_Definer implements Definer_Interface {
 			] ),
 		];
 	}
+
 }
