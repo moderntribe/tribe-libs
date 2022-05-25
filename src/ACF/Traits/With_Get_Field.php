9dd1d2a5b16bd08a3306862dcefc28f0ee195521
Justin Frydman <justin.frydman@gmail.com>
2022-05-24 20:52:46 -0600

    Add With_Get_Field.php ACF trait

---
+++ src/ACF/Traits/With_Get_Field.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Libs\ACF\Traits;
+
+trait With_Get_Field {
+
+	/**
+	 * Retrieve data from an ACF field.
+	 *
+	 * Check to support nullable type properties in components.
+	 * ACF will in some cases return and empty string when we may want it to be null.
+	 * This allows us to always determine the default.
+	 *
+	 * @param  int|string  $key         ACF key identifier.
+	 * @param  mixed       $default     The default value if the field doesn't exist.
+	 * @param  mixed       $identifier  The ACF Identifier, e.g. post_id.
+	 *
+	 * @return mixed
+	 */
+	public function get( $key, $default = false, $identifier = false ) {
+		$value = get_field( $key, $identifier );
+
+		return ! empty( $value )
+			? $value
+			: $default;
+	}
+
+}
