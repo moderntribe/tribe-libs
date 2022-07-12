0dd19efcc7ea20f41ab2575c05d7f8e6f7433bf5
Justin Frydman <justin.frydman@gmail.com>
2022-07-12 14:35:40 -0600

    Add so wp s1 generate <block> --with-middleware option

--- src/Generators/Block_Generator.php
+++ src/Generators/Block_Generator.php
@@ -6,26 +6,36 @@ use WP_CLI;
 use function WP_CLI\Utils\get_flag_value;
 
 class Block_Generator extends Generator_Command {
+
+	public const OPTION_DRY_RUN         = 'dry-run';
+	public const OPTION_WITH_MIDDLEWARE = 'with-middleware';
+
 	/**
-	 * @var string Path to the theme's directory, with a trailing slash
+	 * @var string Path to the theme's directory, with a trailing slash.
 	 */
 	private $theme_path;
 
+	/**
+	 * @var bool Whether the version of square-one includes block middleware.
+	 */
+	private $supports_middelware;
+
 	public function __construct( File_System $file_system, $src_path, $theme_path ) {
 		parent::__construct( $file_system, $src_path );
-		$this->theme_path = trailingslashit( $theme_path );
+		$this->theme_path          = trailingslashit( $theme_path );
+		// @phpstan-ignore-next-line
+		$this->supports_middelware = method_exists( '\Tribe\Project\Blocks\Types\Base_Model', 'init_data' );
 	}
 
-
-	public function description() {
+	public function description(): string {
 		return __( 'Generates a block type.', 'tribe' );
 	}
 
-	public function command() {
+	public function command(): string {
 		return 'generate block';
 	}
 
-	public function arguments() {
+	public function arguments(): array {
 		return [
 			[
 				'type'        => 'positional',
@@ -35,9 +45,16 @@ class Block_Generator extends Generator_Command {
 			],
 			[
 				'type'        => 'flag',
-				'name'        => 'dry-run',
+				'name'        => self::OPTION_DRY_RUN,
 				'optional'    => true,
 				'description' => __( 'During a dry-run, no files will be written', 'tribe' ),
+				'default'     => false,
+			],
+			[
+				'type'        => 'flag',
+				'name'        => self::OPTION_WITH_MIDDLEWARE,
+				'optional'    => true,
+				'description' => __( 'Automatically adds middleware params to the block config', 'tribe' ),
 				'default'     => false,
 			],
 		];
@@ -48,9 +65,10 @@ class Block_Generator extends Generator_Command {
 		$class_name     = $this->class_name( $args[0] );
 		$component_name = $this->component_name( $args[0] );
 
-		$dry_run = get_flag_value( $assoc_args, 'dry-run', false );
+		$dry_run         = get_flag_value( $assoc_args, self::OPTION_DRY_RUN, false );
+		$with_middleware = get_flag_value( $assoc_args, self::OPTION_WITH_MIDDLEWARE, false );
 
-		$this->make_block_config( $type_name, $class_name, $dry_run );
+		$this->make_block_config( $type_name, $class_name, $dry_run, $with_middleware );
 		$this->make_block_model( $class_name, $component_name, $dry_run );
 		$this->make_block_template( $type_name, $class_name, $component_name, $dry_run );
 		$this->make_component( $component_name, $dry_run );
@@ -70,21 +88,36 @@ class Block_Generator extends Generator_Command {
 	/**
 	 * Generate the Block_Config file for the block type
 	 *
-	 * @param string $name
-	 * @param string $class_name
-	 * @param bool   $dry_run
+	 * @param  string  $name
+	 * @param  string  $class_name
+	 * @param  bool    $dry_run
+	 * @param  bool    $with_middleware
 	 *
 	 * @return void
 	 */
-	private function make_block_config( string $name, string $class_name, bool $dry_run ): void {
-		$directory     = $this->config_directory( $class_name );
-		$file_path     = $directory . $class_name . '.php';
+	private function make_block_config( string $name, string $class_name, bool $dry_run, bool $with_middleware ): void {
+		$directory = $this->config_directory( $class_name );
+		$file_path = $directory . $class_name . '.php';
+
+		if ( $with_middleware && ! $this->supports_middelware ) {
+			WP_CLI::warning( 'Sorry, this version of Square One does not support block middleware.' );
+		}
+
+		$middleware_use_statement = ( $with_middleware && $this->supports_middelware ) ? 'use Tribe\Project\Block_Middleware\Contracts\Has_Middleware_Params' : '';
+		$middleware_interface     = ( $with_middleware && $this->supports_middelware ) ? ' implements Has_Middleware_Params' : '';
+		$middleware_method        = ( $with_middleware && $this->supports_middelware ) ? $this->get_block_config_middleware_method() : '';
+
 		$file_contents = sprintf(
 			file_get_contents( __DIR__ . '/templates/block/config.php.tmpl' ),
 			$class_name,
 			$name,
-			$this->human_name( $class_name )
+			$this->human_name( $class_name ),
+			$middleware_use_statement,
+			$middleware_interface,
+			$middleware_method
 		);
+
+		//$file_contents = mb_convert_encoding( $file_contents, 'UTF-8' );
 
 		if ( $dry_run ) {
 			WP_CLI::log( '[Dry Run] Block config file: ' . $file_path );
@@ -104,8 +137,8 @@ class Block_Generator extends Generator_Command {
 		$directory     = $this->config_directory( $class_name );
 		$file_path     = $directory . $class_name . '_Model.php';
 		// Check for updated Base_Model method for versions of square-one that have block middleware.
-		// @phpstan-ignore-next-line
-		$template_path = method_exists( '\Tribe\Project\Blocks\Types\Base_Model', 'init_data' )
+
+		$template_path = $this->supports_middelware
 			? __DIR__ . '/templates/block/model-35.php.tmpl' // tribe libs 3.5 version
 			: __DIR__ . '/templates/block/model.php.tmpl'; // tribe libs 3.4.x version
 
@@ -182,9 +215,8 @@ class Block_Generator extends Generator_Command {
 	private function class_name( string $name ): string {
 		$name = sanitize_title( $name );
 		$name = str_replace( '-', '_', $name );
-		$name = $this->ucwords( $name );
 
-		return $name;
+		return $this->ucwords( $name );
 	}
 
 	private function human_name( string $class_name ): string {
@@ -195,9 +227,8 @@ class Block_Generator extends Generator_Command {
 
 	private function component_name( string $name ): string {
 		$name = sanitize_title( $name );
-		$name = str_replace( '-', '_', $name );
 
-		return $name;
+		return str_replace( '-', '_', $name );
 	}
 
 	private function controller_namespace( string $name ): string {
@@ -206,5 +237,24 @@ class Block_Generator extends Generator_Command {
 
 	private function controller_classname( string $name ): string {
 		return $this->class_name( $name ) . '_Controller';
+	}
+
+	private function get_block_config_middleware_method(): string {
+		return <<<METHOD
+		
+	/**
+	 * @TODO Provide specific middleware parameters.
+	 *
+	 * @return string[][]
+	 */
+	public function get_middleware_params(): array {
+		return [
+			[
+				'the_middleware_key' => 'the_middleware_data',		
+			],
+		];
+	}
+			
+METHOD;
 	}
 }
