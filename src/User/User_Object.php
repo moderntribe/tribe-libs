144b745cb5f31d05be05c94f4ff1ba735e221f51
Justin Frydman <justin.frydman@gmail.com>
2022-08-12 09:45:56 -0600

    Convert User repo

--- src/User/User_Object.php
+++ src/User/User_Object.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\User;
 
@@ -19,21 +18,19 @@ use Tribe\Libs\Object_Meta\Meta_Repository;
  */
 class User_Object {
 
-	const NAME = 'users';
+	public const NAME = 'users';
 
-	/** @var Meta_Map */
-	protected $meta;
+	protected Meta_Map $meta;
 
-	/** @var integer */
-	protected $user_id = 0;
+	protected int $user_id = 0;
 
 	/**
 	 * User_Object constructor.
 	 *
 	 * @param int           $user_id        The ID of a User.
-	 * @param Meta_Map|null $meta           Meta fields appropriate to a user.
+	 * @param \Tribe\Libs\Object_Meta\Meta_Map|null $meta Meta fields appropriate to a user.
 	 */
-	public function __construct( $user_id = 0, Meta_Map $meta = null ) {
+	public function __construct( int $user_id = 0, ?Meta_Map $meta = null ) {
 		$this->user_id = $user_id;
 		if ( isset( $meta ) ) {
 			$this->meta = $meta;
@@ -42,8 +39,23 @@ class User_Object {
 		}
 	}
 
-	public function __get( $key ) {
-		return $this->get_meta( $key );
+	/**
+	 * Get an instance of the User_Object corresponding
+	 * to the user with the given $user_id
+	 *
+	 * @param int $user_id The ID of an existing user
+	 *
+	 * @return static
+	 */
+	public static function factory( int $user_id ): self {
+		/** @var \Tribe\Libs\Object_Meta\Meta_Repository|null $meta_repo */
+		$meta_repo = apply_filters( Meta_Repository::GET_REPO_FILTER, null );
+
+		if ( empty( $meta_repo ) ) {
+			$meta_repo = new Meta_Repository();
+		}
+
+		return new static( $user_id, $meta_repo->get( self::NAME ) );
 	}
 
 	/**
@@ -51,29 +63,22 @@ class User_Object {
 	 * to this user.
 	 *
 	 * @param string $key
+	 *
 	 * @return mixed
 	 */
-	public function get_meta( $key ) {
+	public function get_meta( string $key ) {
 		$user = sprintf( 'user_%s', $this->user_id );
+
 		return $this->meta->get_value( $user, $key );
 	}
 
 	/**
-	 * Get an instance of the User_Object corresponding
-	 * to the user with the given $user_id
+	 * @param string $key
 	 *
-	 * @param int $user_id The ID of an existing user
-	 * @return static
+	 * @return mixed
 	 */
-	public static function factory( $user_id ) {
-		/** @var Meta_Repository $meta_repo */
-		$meta_repo = apply_filters( Meta_Repository::GET_REPO_FILTER, null );
-		if ( empty( $meta_repo ) ) {
-			$meta_repo = new Meta_Repository();
-		}
-
-		$user = new static( $user_id, $meta_repo->get( self::NAME ) );
-
-		return $user;
+	public function __get( string $key ) {
+		return $this->get_meta( $key );
 	}
+
 }
