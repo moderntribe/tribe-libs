a62d664504c3c1d4735ff39066507d115214477d
Justin Frydman <justin.frydman@gmail.com>
2022-08-12 09:38:18 -0600

    Convert Whoops repo

--- src/Whoops/Whoops_Definer.php
+++ src/Whoops/Whoops_Definer.php
@@ -1,16 +1,17 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\Whoops;
 
 use DI;
 use Tribe\Libs\Container\Definer_Interface;
 use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class Whoops_Definer implements Definer_Interface {
+
 	public function define(): array {
 		return [
-			\Whoops\Run::class => DI\autowire()
+			Run::class => DI\autowire()
 				->constructor( null )
 				->method( 'pushHandler', DI\get( PrettyPageHandler::class ) ),
 		];
