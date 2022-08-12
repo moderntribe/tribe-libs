4e2c69e7eb8b0b08b796281ea24e12c405599700
Justin Frydman <justin.frydman@gmail.com>
2022-08-12 09:49:02 -0600

    Convert Twig repo

--- src/Twig/Extension.php
+++ src/Twig/Extension.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\Twig;
 
@@ -7,7 +7,8 @@ use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class Extension extends AbstractExtension {
-	public function getFilters() {
+
+	public function getFilters(): array {
 		return apply_filters( 'tribe/libs/twig/filters', [
 			new TwigFilter( 'strip_shortcodes', 'strip_shortcodes' ),
 			new TwigFilter( 'wp_trim_words', 'wp_trim_words' ),
@@ -15,9 +16,7 @@ class Extension extends AbstractExtension {
 			new TwigFilter( 'sanitize_html_class', 'sanitize_html_class' ),
 			new TwigFilter( 'sanitize_title', 'sanitize_title' ),
 			new TwigFilter( 'wpautop', 'wpautop' ),
-			new TwigFilter( 'apply_filters', static function ( $tag, $args ) {
-				return apply_filters_ref_array( $tag, $args );
-			}, [ 'is_variadic' => true ] ),
+			new TwigFilter( 'apply_filters', static fn($tag, $args) => apply_filters_ref_array( $tag, $args ), [ 'is_variadic' => true ] ),
 			new TwigFilter( 'esc_url', 'esc_url' ),
 			new TwigFilter( 'esc_attr', 'esc_attr' ),
 			new TwigFilter( 'esc_html', 'esc_html' ),
@@ -26,7 +25,7 @@ class Extension extends AbstractExtension {
 		] );
 	}
 
-	public function getFunctions() {
+	public function getFunctions(): array {
 		return apply_filters( 'tribe/libs/twig/funcitons', [
 			new TwigFunction( 'do_action', 'do_action' ),
 			new TwigFunction( 'do_shortcode', 'do_shortcode' ),
@@ -35,6 +34,5 @@ class Extension extends AbstractExtension {
 			} ),
 		] );
 	}
-
 
 }
