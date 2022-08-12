96aa22020774be9d746efadcb852a27dac094a96
Justin Frydman <justin.frydman@gmail.com>
2022-08-12 12:14:22 -0600

    Convert Required_Page repo

--- src/Required_Page/Required_Page_Definer.php
+++ src/Required_Page/Required_Page_Definer.php
@@ -1,16 +1,21 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Libs\Required_Page;
 
+use DI;
 use Tribe\Libs\Container\Definer_Interface;
 
 class Required_Page_Definer implements Definer_Interface {
+
 	public const PAGES = 'libs.required_page.pages';
 
+	/**
+	 * @return mixed[]
+	 */
 	public function define(): array {
 		return [
-			self::PAGES => \DI\add( [] ),
+			self::PAGES => DI\add( [] ),
 		];
 	}
+
 }
