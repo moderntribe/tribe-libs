6b8cf50adbeb96ac55bd5c425b7ed7bbcf596219
Justin Frydman <justin.frydman@gmail.com>
2022-08-15 17:35:25 -0600

    Allow test installer to install plugins only, let CI download the different versions of WordPress

--- tests/bin/setup-tests.php
+++ tests/bin/setup-tests.php
@@ -14,14 +14,23 @@ $downloaders = [
 	new Plugin_Downloader( 'posts-to-posts' ),
 ];
 
+$options = getopt( '', [
+	'plugins-only',
+] );
+
+if ( isset( $options['plugins-only'] ) ) {
+    echo "Skipping WordPress installation..." . PHP_EOL;
+    unset( $downloaders[0] );
+}
+
 ( new Installer( $downloaders ) )->install();
 
 $tests_dist_file = __DIR__ . '/../.env-dist';
 $tests_dest_file = __DIR__ . '/../.env';
 
 if ( ! file_exists( $tests_dest_file ) ) {
-    echo "copying $tests_dist_file to $tests_dest_file" . PHP_EOL;
-    copy( $tests_dist_file, $tests_dest_file );
+	echo "copying $tests_dist_file to $tests_dest_file" . PHP_EOL;
+	copy( $tests_dist_file, $tests_dest_file );
 }
 
 echo 'Done!';
